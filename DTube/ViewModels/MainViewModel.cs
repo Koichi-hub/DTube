ac888ddaeb6e7ae798397f67f66ed934620c2fe0
TDavydov <gogotimur2020@gmail.com>
2025-03-30 02:48:41 +0500

    Добавил лоадер и сайдбар

--- DTube/ViewModels/MainViewModel.cs
+++ DTube/ViewModels/MainViewModel.cs
@@ -1,6 +1,44 @@
-﻿namespace DTube.ViewModels;
+﻿using ReactiveUI;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DTube.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string? searchText = "";
+    public string? SearchText
+    {
+        get => searchText;
+        set => this.RaiseAndSetIfChanged(ref searchText, value);
+    }
+
+    private bool isLoaderVisible;
+    public bool IsLoaderVisible
+    {
+        get => isLoaderVisible;
+        set => this.RaiseAndSetIfChanged(ref isLoaderVisible, value);
+    }
+
+    #region Commands
+    public ICommand SearchCommand { get; }
+    #endregion
+
+    public MainViewModel()
+    {
+        SearchCommand = ReactiveCommand.Create(Search);
+    }
+
+    public async Task Search()
+    {
+        if (IsLoaderVisible)
+            return;
+
+        IsLoaderVisible = true;
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        IsLoaderVisible = false;
+    }
 }
