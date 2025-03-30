71970789209946d7d365aab2d00e2e61d47784fe
TDavydov <gogotimur2020@gmail.com>
2025-03-30 15:32:46 +0500

    Привязал вью медиа к модели. Добавил отображение списка. Добавил фильтрацию медиа. Добавил блок с кнопками для скачивания

---
+++ DTube/Models/MediaModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DTube.Models
+{
+    public class MediaModel
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public MediaType Type { get; set; }
+        public long SizeInBytes { get; set; }
+        public int Duration { get; set; }
+
+        public enum MediaType
+        {
+            Video = 0, 
+            Music = 1,
+        }
+    }
+}
--- DTube/ViewModels/MainViewModel.cs
+++ DTube/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
-﻿using ReactiveUI;
+﻿using DTube.Models;
+using ReactiveUI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -21,6 +24,96 @@ public class MainViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref isLoaderVisible, value);
     }
 
+    private bool isMediaBlockVisible = true;
+    public bool IsMediaBlockVisible
+    {
+        get => isMediaBlockVisible;
+        set => this.RaiseAndSetIfChanged(ref isMediaBlockVisible, value);
+    }
+
+    private bool isVideoFilter;
+    public bool IsVideoFilter
+    {
+        get => isVideoFilter;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref isVideoFilter, value);
+            FilterMedia();
+        }
+    }
+
+    private bool isMusicFilter;
+    public bool IsMusicFilter
+    {
+        get => isMusicFilter;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref isMusicFilter, value);
+            FilterMedia();
+        }
+    }
+
+    private MediaModel mediaModel = new()
+    {
+        Title = "Title",
+        Description = "Description",
+    };
+    public MediaModel MediaModel
+    {
+        get => mediaModel;
+        set => this.RaiseAndSetIfChanged(ref mediaModel, value);
+    }
+
+    private readonly static List<MediaModel> mediaModelsData = [
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Video,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Video,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Video,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Video,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Video,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Music,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Music,
+        },
+        new MediaModel {
+            Title = "Title",
+            Description = "Description",
+            Type = MediaModel.MediaType.Music,
+        },
+        ];
+
+    private List<MediaModel> mediaModels = mediaModelsData;
+    public List<MediaModel> MediaModels
+    {
+        get => mediaModels;
+        set => this.RaiseAndSetIfChanged(ref mediaModels, value);
+    }
+
     #region Commands
     public ICommand SearchCommand { get; }
     #endregion
@@ -40,5 +133,17 @@ public class MainViewModel : ViewModelBase
         await Task.Delay(TimeSpan.FromSeconds(1));
 
         IsLoaderVisible = false;
+    }
+
+    private void FilterMedia()
+    {
+        if (IsVideoFilter == IsMusicFilter)
+        {
+            MediaModels = mediaModelsData;
+        } 
+        else
+        {
+            MediaModels = mediaModelsData.Where(x => (IsVideoFilter && x.Type == MediaModel.MediaType.Video) || (IsMusicFilter && x.Type == MediaModel.MediaType.Music)).ToList();
+        }
     }
 }
